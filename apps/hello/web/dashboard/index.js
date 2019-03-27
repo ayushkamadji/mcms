1558fcb19dc5f13675c69985a127ae4581938d1c
Ayushka Partohap <partohap.ayushka@gmail.com>
2019-03-27 14:10:08 +0700

    Add example dashboards

---
+++ apps/hello/web/dashboard/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HelloDash extends Component {
+  render() {
+    return (
+      <div>
+        From Hello
+      </div>
+    )
+  }
+}
+
+export default HelloDash
