5e361a6a12bad41d085ef5f797695f351a75c756
Ayushka Partohap <partohap.ayushka@gmail.com>
2019-04-06 03:05:44 +0700

    Add static app

--- apps/blog/web/routes.js
+++ apps/blog/web/routes.js
@@ -1,6 +1,7 @@
 const routes = ({Router, access, Controllers: {PostsController}}) => {
   const router = Router()
 
+  router.get("/", (req, res) => res.redirect("/blog/posts"))
   router.get("/posts", PostsController.index)
   router.post("/posts", access.authorize("SITE_OWNER"), PostsController.create)
   router.get("/posts/:id", PostsController.show)
