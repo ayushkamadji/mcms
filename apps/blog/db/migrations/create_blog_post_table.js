aaf8142f8bf7767b0c752b897b34f9e940a7a13b
Ayushka Partohap <partohap.ayushka@gmail.com>
2019-04-09 15:56:50 +0700

    Add hardcode blog post demo data

--- apps/blog/db/migrations/create_blog_post_table.js
+++ apps/blog/db/migrations/create_blog_post_table.js
@@ -10,6 +10,15 @@ exports.up = (db) => {
       table.string("image_url")
       table.timestamps(null, true)
     })
+    .then( () => {
+      const body = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam id libero hic eum a expedita laudantium, alias reprehenderit culpa, at sequi doloribus fuga officia nisi itaque nostrum impedit explicabo est!"
+      return db("blog_post")
+        .returning("*")
+        .insert([
+          { title: "My First Post", body, image_url: "sample.png"},
+          { title: "Another Post", body, image_url: "sample_2.png"},
+        ])
+    })
 }
 
 exports.down = (db) => {
