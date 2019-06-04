0395dac9cd9d4d8925267c1d4f3174ef95ad70a9
Ayushka Partohap <partohap.ayushka@gmail.com>
2019-06-04 17:47:29 +0700

    Fix webpack build and dev server start.
    - Add sass-loader
    - Fix dist folder ignore
    - Change some npm scripts

--- system.config.js
+++ system.config.js
@@ -1,14 +1,16 @@
-const appsDir = "./apps"
-const appNames = [ "blog" ]
-
 const users = require("./lib/core/access/config")
+const blog = require("./apps/blog/config")
 
-const apps = appNames.reduce( (acc, appName) => {
-  return Object.assign(acc, {
-    [appName]: require(`${appsDir}/${appName}/config`)
-  })
-}, { users })
+// Dynamic require is causing problems, need to check dependency update changes
+
+// const appsDir = "./apps"
+// const appNames = [ "blog" ]
+// const apps = appNames.reduce( (acc, appName) => {
+//   return Object.assign(acc, {
+//     [appName]: require(`${appsDir}/${appName}/config`)
+//   })
+// }, { users })
 
 module.exports = {
-  apps
+  apps: { users, blog }
 }
