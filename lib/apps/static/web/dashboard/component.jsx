7148ce860bac585da7bde2af68b578abbba6a893
Wahyu Haryanto <way3haryanto@gmail.com>
2019-06-15 13:55:25 +0700

    Upload and preview image
    - handler upload

--- lib/apps/static/web/dashboard/component.jsx
+++ lib/apps/static/web/dashboard/component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AddImageIcon from './assets/icon_addimage.png';
 import styles from './styles.scss';
 
 class GalleryComponent {
@@ -21,7 +22,32 @@ class GalleryComponent {
 
   addImage() {
     return (
-      <div className={styles.add_image}>Add images</div>
+      <div className={styles.add_image}>
+        {this.buttonUpload()}
+        {this.previewImage()}
+      </div>
+    );
+  }
+
+  buttonUpload() {
+    const { onSelectImage, imagePreviewUrl } = this.props;
+    if (imagePreviewUrl) return null;
+    return (
+      <input
+        type="file"
+        name="newImage"
+        onChange= {onSelectImage}
+      />
+    );
+  }
+
+  previewImage() {
+    const { imagePreviewUrl } = this.props;
+    if (!imagePreviewUrl) return null;
+    return (
+      <div className={styles.preview_image}>
+        <img src={imagePreviewUrl} />
+      </div>
     );
   }
 
@@ -29,17 +55,18 @@ class GalleryComponent {
     const { galleries } = this.props;
     return (
       <div className={styles.gallery_list}>
-        {galleries.map(gallery => this.galleryItem(gallery.file))}
+        {galleries.map(gallery => this.galleryItem(gallery))}
       </div>
     );
   }
 
   galleryItem(gallery) {
+    const { deleteImage } = this.props;
     return (
       <div className={styles.gallery_item}>
-        <img src={`/public/assets/${gallery}`} />
+        <img src={`/public/assets/${gallery.file}`} />
         <div className={styles.delete_section}>
-          <button />
+          <button onClick={() => deleteImage(gallery)} />
         </div>
       </div>
     );
--- lib/apps/static/web/dashboard/index.jsx
+++ lib/apps/static/web/dashboard/index.jsx
@@ -6,14 +6,62 @@ class Gallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      galleries: imagesJson
+      galleries: imagesJson,
+      file: '',
+      imagePreviewUrl: ''
     };
+  }
+
+  onSelectImage(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+
+      // after uploaded, move it latter
+      this.uploadImage({
+        id: 99,
+        name: '',
+        file: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file);
+  }
+
+  uploadImage(newImage) {
+    console.log('uploading image...')
+    const newGallery = [newImage].concat(this.state.galleries)
+    setTimeout(
+      () => this.setState({ galleries: newGallery, file: '', imagePreviewUrl: '' }),
+      1000
+    );
+
+  }
+
+  deleteImage(gallery) {
+    const { galleries } = this.state;
+    const { id, name } = gallery;
+    const confirmDelete = confirm(`Hapus ${name}?`);
+    if (confirmDelete) {
+      console.log(`do delete ${id}`);
+      const restGalleries = galleries.filter(objGallery => objGallery.id !== gallery.id);
+      this.setState({ galleries: restGalleries })
+    }
   }
 
   render() {
     const props = {
       ...this.props,
-      galleries: this.state.galleries
+      ...this.state,
+      onSelectImage: this.onSelectImage.bind(this),
+      deleteImage: this.deleteImage.bind(this)
     };
     return GalleryComponent(props);
   }
